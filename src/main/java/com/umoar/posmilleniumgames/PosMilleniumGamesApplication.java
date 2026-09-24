b2856f6021e27946c7433cd5954116aa1633af9e
Marvin Alexander Coto Hernandez <libroypan1234@gmail.com>
2026-09-24 14:21:08 -0600

    Initialize commit for SpringBoot project

---
+++ src/main/java/com/umoar/posmilleniumgames/PosMilleniumGamesApplication.java
@@ -0,0 +1,13 @@
+package com.umoar.posmilleniumgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosMilleniumGamesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PosMilleniumGamesApplication.class, args);
+    }
+
+}
